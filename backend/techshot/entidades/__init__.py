de99e839fda5f5f8ccc29bb1c6a6abb97f9a8ad2
Prof. Tavares <leodaniel@gmail.com>
2023-01-31 21:41:05 -0300

    Lint

--- backend/techshot/entidades/__init__.py
+++ backend/techshot/entidades/__init__.py
@@ -1,3 +1,6 @@
+"""
+Módulo que contém as entidades do sistema.
+"""
 from techshot.entidades.usuario import UsuarioCriacao
 from techshot.entidades.informacao_pessoal import InformacaoPessoalCriacao
 from techshot.entidades.postagem import Postagem

4d4ed203059d395b50e5f6c3c4b13e411d1cc077
Felipe Augusto Cardoso Pereira <felipe.augusto@enacom.com.br>
2023-02-02 09:03:49 -0300

    inicio implementacao teste

--- backend/test/api/test_usuario.py
+++ backend/test/api/test_usuario.py
@@ -5,16 +5,50 @@ from fastapi.testclient import TestClient
 from techshot.orm.base import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI
+from techshot.orm.database import get_session
+
+# cria uma instância de engine
+engine = create_engine('sqlite:///:memory:', echo=True)
+# cria as tabelas necessárias
+Base.metadata.create_all(bind=engine)
+# cria uma instância de sessionmaker
+Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_session_new():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @fixture(scope='module')
-def session():
-    # cria uma instância de engine
-    engine = create_engine('sqlite:///:memory:', echo=True)
-    # cria as tabelas necessárias
-    Base.metadata.create_all(bind=engine)
+def client():
+    app = FastAPI()
+    api_usuario = ApiUsuario()
+    app.include_router(api_usuario.router)
+    app.dependency_overrides[get_session] = get_session_new
+    return TestClient(app)
 
-    # cria uma instância de sessionmaker
-    Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-    # cria uma instância de sessão
-    session = Session()
-    return session
+def test_crud_usuario_api(client):
+    #add
+    response = client.post('/usuarios', json={"nome": "Joao", 
+                                   "nome_usuario": "jao"})
+    assert response.status_code == 200
+    #search not found
+    response = client.get('/usuarios/xxxx')
+    assert response.status_code == 404
+    assert response.json() == { 
+        "detail": "Usuário não encontrado" }
+    #search found
+    response = client.get('/usuarios/jao')
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data['nome'] == "Joao"
+    assert response_data['nome_usuario'] == 'jao'
+    assert response_data['versao'] == 1
+
+
+    
+
+

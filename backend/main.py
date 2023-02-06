9b19daf0788912a2d5fea9a49dce427b8687c098
enacom <arthur.ribeiro@enacom.com.br>
2023-02-06 14:59:23 -0300

    criação dos testes de unidade

--- backend/main.py
+++ backend/main.py
@@ -2,5 +2,4 @@ from techshot.api.usuario import UsuarioApi
 from fastapi import FastAPI
 
 app = FastAPI()
-api_usuario = UsuarioApi()
-app.include_router(app.router)
+app.include_router(UsuarioApi().router)

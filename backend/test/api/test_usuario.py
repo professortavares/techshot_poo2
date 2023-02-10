14cae8509ba616cbb0793759128384f82f1864e4
Felipe Augusto Cardoso Pereira <felipe.augusto@enacom.com.br>
2023-02-10 08:32:00 -0300

    finalizando teste

--- backend/test/api/test_usuario.py
+++ backend/test/api/test_usuario.py
@@ -2,7 +2,7 @@ import pytest
 from pytest import fixture
 from techshot.api import ApiUsuario
 from fastapi.testclient import TestClient
-from techshot.orm.usuario import Usuario
+from techshot.orm.base import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from fastapi import FastAPI
@@ -15,12 +15,11 @@ import os
 @fixture
 def client():
     # cria uma instância de engine
-    engine = create_engine('sqlite:///:memory:', echo=True)
+    engine = create_engine('sqlite:///temp.db', echo=True)
     # cria as tabelas necessárias
-    Usuario.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     # cria uma instância de sessionmaker
     session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-    
     def get_session_new():
         db = session()
         try:
@@ -32,8 +31,8 @@ def client():
     api_usuario = ApiUsuario()
     app.include_router(api_usuario.router)
     app.dependency_overrides[get_session] = get_session_new
-    return TestClient(app)
-
+    yield TestClient(app)
+    Base.metadata.drop_all(bind=engine)
 
 def test_crud_usuario_api(client):
     #add
@@ -52,8 +51,18 @@ def test_crud_usuario_api(client):
     assert response_data['nome'] == "Joao"
     assert response_data['nome_usuario'] == 'jao'
     assert response_data['versao'] == 1
-
-
-    
-
-
+    #put
+    response = client.put('/usuarios/jao', json={"nome": "João", 
+                                                 "nome_usuario": "jao"})
+    assert response.status_code == 200
+    response = client.get('/usuarios/jao')
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data['nome'] == "João"
+    assert response_data['nome_usuario'] == 'jao'
+    assert response_data['versao'] == 2
+    #delete
+    response = client.delete('/usuarios/jao')
+    assert response.status_code == 200
+    response = client.get('/usuarios/jao')
+    assert response.status_code == 404
